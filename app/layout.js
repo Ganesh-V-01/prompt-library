e845a39e6279d8d48784907b81dde9395392fbff
Techno Fusion Club Developer <gv929@example.com>
2026-06-08 16:41:41 +0530

    Fix metadata bug

--- app/layout.js
+++ app/layout.js
@@ -3,13 +3,10 @@ import './globals.css';
 import * as Sentry from '@sentry/nextjs';
 import { PostHogProvider } from './providers';
 
-export const metadata = {
-  title: 'PromptGram | The Ultimate AI Prompt Library',
-  description: 'Copy, paste, and generate high-quality AI images instantly.',
-};
-
 export async function generateMetadata() {
   return {
+    title: 'PromptGram | The Ultimate AI Prompt Library',
+    description: 'Copy, paste, and generate high-quality AI images instantly.',
     other: {
       ...Sentry.getTraceData()
     }

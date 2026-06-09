92a9225e2d2b458be721005617ebe5e90507407a
Techno Fusion Club Developer <gv929@example.com>
2026-06-09 11:23:35 +0530

    Fix missing Google fonts in layout

--- app/layout.js
+++ app/layout.js
@@ -3,6 +3,11 @@ import * as Sentry from '@sentry/nextjs';
 import { PostHogProvider } from './providers';
 import Sidebar from './components/Sidebar';
 import MobileHeader from './components/MobileHeader';
+import { Playfair_Display, DM_Sans, JetBrains_Mono } from 'next/font/google';
+
+const playfair = Playfair_Display({ subsets: ['latin'], variable: '--font-playfair' });
+const dmSans = DM_Sans({ subsets: ['latin'], variable: '--font-dm-sans' });
+const jetbrains = JetBrains_Mono({ subsets: ['latin'], variable: '--font-jetbrains' });
 
 export async function generateMetadata() {
   return {
